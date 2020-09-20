c7f690554fc9e57f9cc8b8f660e8038b5ddfe33d
Yuuki <30376282+Yuuki795@users.noreply.github.com>
2020-09-20 19:28:40 -0400

    Update command.js

--- commands/command.js
+++ commands/command.js
@@ -1,1 +1,12 @@
 
+
+//DO NOT REMOVE THESE THEY ARE REQUIRED FOR ALL COMMANDS
+module.exports = class test {
+    constructor() {
+        this.name = ''
+        this.alias = [''],
+            this.usage = ``
+    }
+
+    async run(bot, text, args) {
+    }

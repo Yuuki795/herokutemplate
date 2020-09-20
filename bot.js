03619ab658a846c7cf64ca43a009946f0b568264
Yuuki <30376282+Yuuki795@users.noreply.github.com>
2020-09-20 19:26:54 -0400

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,35 @@
+const Discord = require('discord.js');
+const botconfig = require("./util/botconfig.json");
+const bot = new Discord.Client({ disableEveryone: true });
+const { CommandHandler } = require("djs-commands")
+const client = require('nekos.life');
+
+const CH = new CommandHandler({
+    folder: __dirname + "/commands/",
+    prefix: [botconfig.prefix]
+});
+
+
+bot.on("ready", () => {
+    console.log(`Logged in as ${bot.user.tag} on ${bot.guilds.cache.size} servers`)
+})
+
+bot.on("message", (message) => {
+    // if(message.channel.type === "dm") return;
+    if (message.author.type === "bot") return;
+
+    let args = message.content.split(" ");
+    let command = args[0];
+    let cmd = CH.getCommand(command);
+
+    if (!cmd) return;
+
+    try {
+        cmd.run(bot, message, args)
+    } catch (e) {
+        console.log(e)
+    }
+});
+
+
+bot.login(botconfig.token);
